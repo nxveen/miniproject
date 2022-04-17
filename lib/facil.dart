853708b3467af6d0e5b806bf7637e667c101bfc8
Rejeesh2001 <jeesh2001@gmail.com>
2022-04-17 21:28:41 +0530

    9th commit

--- lib/facil.dart
+++ lib/facil.dart
@@ -3,40 +3,29 @@ import 'package:miniproject/bikaroo.dart';
 import 'package:miniproject/login.dart';
 import 'package:miniproject/fuel.dart';
 import 'package:miniproject/jin.dart';
-<<<<<<< HEAD
-import 'dart:convert';
-=======
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+
 class Facility extends StatelessWidget {
   final Barcode? text;
-  Facility({Key? key,required this.text}
-      ) : super(key: key);
+  Facility({Key? key, required this.text}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var id='';
-<<<<<<< HEAD
-    var petrol_pumb='';
-=======
-    var petrol_pump='';
-    var  auto1='';
-    var p1='';
-    var  auto2='';
-    var p2='';
-    var  mech1='';
-    var p3='';
-    var  mech2='';
-    var p4='';
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-    String? Data='';
-    if(text != null) {
+    var id = '';
+    var petrol_pump = '';
+    var auto1 = '';
+    var p1 = '';
+    var auto2 = '';
+    var p2 = '';
+    var mech1 = '';
+    var p3 = '';
+    var mech2 = '';
+    var p4 = '';
+    String? Data = '';
+    if (text != null) {
       Data = text!.code;
-      if(Data!=null) {
+      if (Data != null) {
         var value_list = Data.split(",");
         id = value_list[0].split("=")[1];
-<<<<<<< HEAD
-        petrol_pumb = value_list[1].split("=")[1];
-=======
         petrol_pump = value_list[1].split("=")[1];
         auto1 = value_list[2].split("=")[1];
         p1 = value_list[3].split("=")[1];
@@ -46,124 +35,112 @@ class Facility extends StatelessWidget {
         p3 = value_list[7].split("=")[1];
         mech2 = value_list[8].split("=")[1];
         p4 = value_list[9].split("=")[1];
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
       }
-    }
-    else{
+    } else {
       Data = 'abc';
-      id='';
+      id = '';
     }
-<<<<<<< HEAD
-    double w = MediaQuery.of(context).size.width;
-=======
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
         body: Stack(children: [
-          Container(
-            decoration:const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/page3.jpg'),
-                fit: BoxFit.fill,
-              ),
-            ),
+      Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/page3.jpg'),
+            fit: BoxFit.fill,
           ),
-          TextButton(onPressed: () {Navigator.push(context,
-            MaterialPageRoute(builder:(context) => Login()),);},
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(h*0.007, h*0.03,0 ,0),
-              child: Icon(
-                Icons.arrow_back_ios,
-                color: Colors.white,
-                size: h*0.03,
-              ),
-            ),
+        ),
+      ),
+      TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Login()),
+          );
+        },
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(h * 0.007, h * 0.03, 0, 0),
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+            size: h * 0.03,
           ),
-          Column(
-            children: [
-              SizedBox(height: h*0.11),
-              Center(
-                child: TextButton(
-                  onPressed: () {Navigator.push(context,
-                    MaterialPageRoute(builder:(context) => FavoriteWidget(id: id)),);},
-                  child:Column(
-                    children: [
-                      Image.asset('assets/img1.jpg'),
-
-<<<<<<< HEAD
-                      Text('BIKAROO $id',
-=======
-                      Text('BIKAROO ',
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-                            style: TextStyle(
-                              fontSize: h*(0.12/5.0),
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: h*(0.1/60.0),
-                              color: Colors.white,
-                              fontFamily: 'Poppins',
-                        )
-                      )
-                    ]
-                  )
-                ),
-              ),
-              SizedBox(height: h*0.11),
-              Center(
-                child: TextButton(
-                    onPressed: () {Navigator.push(context,
-                      MaterialPageRoute(builder:(context) => Fuel(petrol_pump: petrol_pump,mech1: mech1,p3: p3,mech2: mech2,p4: p4)),);},
-                    child:Column(
-                        children: [
-                          Image.asset('assets/img2.jpg'),
-                          Text('FUEL DELIVERY / VEHICLE REPAIR',
-                              style: TextStyle(
-                                fontSize: h*(0.12/5.0),
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: h*(0.1/60.0),
-                                color: Colors.white,
-                                fontFamily: 'Poppins',
-                              )
-                          )
-                        ]
-                    )
-                ),
-              ),
-              SizedBox(height: h*0.12),
-              Center(
-<<<<<<< HEAD
-                child: FlatButton(
-                    onPressed: () {Navigator.push(context,
-                      MaterialPageRoute(builder:(context) => Jin()),);},
-=======
-                child: TextButton(
-                    onPressed: () {Navigator.push(context,
-                      MaterialPageRoute(builder:(context) => Jin(auto1: auto1,p1: p1,auto2: auto2,p2: p2)),);},
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-                    child:Column(
-                        children: [
-                          Image.asset('assets/vector.png'),
-                          Text('JINRICKSHAW',
-                              style: TextStyle(
-                                fontSize: h*(0.12/5.0),
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: h*(0.1/60.0),
-                                color: Colors.white,
-                                fontFamily: 'Poppins',
-                              )
-                          )
-                        ]
-                    )
-                ),
-              ),
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-            ]
-          )
-        ]
-        )
-    );
+        ),
+      ),
+      Column(children: [
+        SizedBox(height: h * 0.11),
+        Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => FavoriteWidget(id: id)),
+                );
+              },
+              child: Column(children: [
+                Image.asset('assets/img1.jpg'),
+                Text('BIKAROO ',
+                    style: TextStyle(
+                      fontSize: h * (0.12 / 5.0),
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: h * (0.1 / 60.0),
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ))
+              ])),
+        ),
+        SizedBox(height: h * 0.11),
+        Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Fuel(
+                          petrol_pump: petrol_pump,
+                          mech1: mech1,
+                          p3: p3,
+                          mech2: mech2,
+                          p4: p4)),
+                );
+              },
+              child: Column(children: [
+                Image.asset('assets/img2.jpg'),
+                Text('FUEL DELIVERY / VEHICLE REPAIR',
+                    style: TextStyle(
+                      fontSize: h * (0.12 / 5.0),
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: h * (0.1 / 60.0),
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ))
+              ])),
+        ),
+        SizedBox(height: h * 0.12),
+        Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          Jin(auto1: auto1, p1: p1, auto2: auto2, p2: p2)),
+                );
+              },
+              child: Column(children: [
+                Image.asset('assets/vector.png'),
+                Text('JINRICKSHAW',
+                    style: TextStyle(
+                      fontSize: h * (0.12 / 5.0),
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: h * (0.1 / 60.0),
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ))
+              ])),
+        ),
+      ])
+    ]));
   }
 }
--- lib/fuel.dart
+++ lib/fuel.dart
@@ -1,112 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:miniproject/fuel2.dart';
 import 'package:miniproject/repair.dart';
+
 class Fuel extends StatelessWidget {
-  var petrol_pump='';
-  var mech1='';
-  var p3='';
-  var mech2='';
-  var p4='';
-  Fuel({Key? key,required this.petrol_pump,required this.mech1,required this.p3,required this.mech2,required this.p4}
-      ) : super(key: key);
+  var petrol_pump = '';
+  var mech1 = '';
+  var p3 = '';
+  var mech2 = '';
+  var p4 = '';
+  Fuel(
+      {Key? key,
+      required this.petrol_pump,
+      required this.mech1,
+      required this.p3,
+      required this.mech2,
+      required this.p4})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Stack(children: [
+        body: Stack(children: [
       Container(
-      decoration:const BoxDecoration(
-      image: DecorationImage(
-          image: AssetImage('assets/fuel.jpg'),
-      fit: BoxFit.fill,
-    ),
-    ),
-    ),
-<<<<<<< HEAD
-        FlatButton(onPressed: () {},
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(h*0.007, h*0.03,0 ,0),
-            child: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-              size: h*0.03,
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fuel.jpg'),
+            fit: BoxFit.fill,
           ),
         ),
-=======
-
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-        Column(
-          children: [
-            SizedBox(height: h*0.45),
-            Center(
-              child: TextButton(onPressed: () {Navigator.push(context,
-                MaterialPageRoute(builder:(context) => Fuel2(petrol_pump: petrol_pump,mech1: mech1,p3: p3,mech2: mech2,p4: p4)),);},
-                child: Container(
-                    height: h*0.076,
-                    width: w*0.85,
-                    decoration:const BoxDecoration(color: Color.fromRGBO(191, 90, 242, 1),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
+      ),
+      Column(children: [
+        SizedBox(height: h * 0.45),
+        Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Fuel2(
+                        petrol_pump: petrol_pump,
+                        mech1: mech1,
+                        p3: p3,
+                        mech2: mech2,
+                        p4: p4)),
+              );
+            },
+            child: Container(
+                height: h * 0.076,
+                width: w * 0.85,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(191, 90, 242, 1),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Center(
+                  child: Text('FUEL DELIVERY',
+                      style: TextStyle(
+                        fontSize: h * (0.12 / 5.0),
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: h * (0.05 / 60.0),
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                      )),
+                )),
+          ),
+        ),
+        SizedBox(height: h * 0.08),
+        Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Repair(
+                        petrol_pump: petrol_pump,
+                        mech1: mech1,
+                        p3: p3,
+                        mech2: mech2,
+                        p4: p4)),
+              );
+            },
+            child: Container(
+                height: h * 0.076,
+                width: w * 0.85,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color.fromRGBO(191, 90, 242, 1),
+                        Color.fromRGBO(191, 90, 242, 1),
+                      ],
                     ),
-                    child: Center(
-                      child: Text(
-                          'FUEL DELIVERY',
-                          style: TextStyle(
-                            fontSize: h*(0.12/5.0),
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: h*(0.05/60.0),
-                            color: Colors.white,
-                            fontFamily: 'Poppins',
-                          )
-                      ),
-                    )
-                ),
-              ),
-            ),
-            SizedBox(height: h*0.08),
-            Center(
-<<<<<<< HEAD
-              child: FlatButton(onPressed: () {Navigator.push(context,
-                MaterialPageRoute(builder:(context) => Repair()),);},
-=======
-              child: TextButton(onPressed: () {Navigator.push(context,
-                MaterialPageRoute(builder:(context) => Repair(petrol_pump: petrol_pump,mech1: mech1,p3: p3,mech2: mech2,p4: p4)),);},
->>>>>>> 4fd2db2ca56b11ba9da8a3ad36bb2f8f29162474
-                child: Container(
-                    height: h*0.076,
-                    width: w*0.85,
-                    decoration:const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [
-                            Color.fromRGBO(191, 90, 242, 1),
-                            Color.fromRGBO(191, 90, 242, 1),
-
-                          ],
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
-                    ),
-                    child: Center(
-                      child: Text(
-                          'VEHICLE REPAIRS',
-                          style: TextStyle(
-                            fontSize: h*(0.12/5.0),
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: h*(0.05/60.0),
-                            color: Colors.white,
-                            fontFamily: 'Poppins',
-                          )
-                      ),
-                    )
-                ),
-              ),
-            ),
-          ]
-        )
-    ]
-    )
-    );
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Center(
+                  child: Text('VEHICLE REPAIRS',
+                      style: TextStyle(
+                        fontSize: h * (0.12 / 5.0),
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: h * (0.05 / 60.0),
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                      )),
+                )),
+          ),
+        ),
+      ])
+    ]));
   }
 }
